8374fcb462f6b0437829831d6482364d74a7c6f9
Jorge Contreras <jorge@lecontre.co.nz>
2023-04-09 21:18:21 +1200

    Top Navigation and Main Menu Layout

--- POS_App/POS_UI/POSClient/src/App.tsx
+++ POS_App/POS_UI/POSClient/src/App.tsx
@@ -1,62 +1,14 @@
 import React, { useState } from 'react';
-import ProductList from "./Components/ProductList";
-import './App.css';
+import MainNavigation from "./Components/Layout/MainNavigation";
 
-const products = [
-  {
-    id: 1,
-    name: 'Product 1',
-    description: 'This is the description of product 1',
-    price: 19.99,
-    image: 'https://via.placeholder.com/150',
-  },
-  {
-    id: 2,
-    name: 'Product 2',
-    description: 'This is the description of product 2',
-    price: 29.99,
-    image: 'https://via.placeholder.com/150',
-  },
-  // ... more products
-];
+import './App.scss';
+
 
 const App: React.FC = () => {
-  const [input, setInput] = useState<string>("");
-  const [result, setResult] = useState<string>("");
-
-  const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    setInput(input + e.currentTarget.value);
-  };
-
-  const calculateResult = () => {
-    try {
-      setResult(eval(input));
-    } catch (error) {
-      setResult("Error");
-    }
-  };
-
-  const clearInput = () => {
-    setInput("");
-    setResult("");
-  };
 
   return (
-      <div className="App">
-        <h1>React Calculator</h1>
-        <div className="calculator">
-          <input type="text" readOnly value={input} />
-          <input type="text" readOnly value={result} />
-          <div className="buttons">
-            {['7', '8', '9', '4', '5', '6', '1', '2', '3', '0', '.', '+', '-', '*', '/'].map((value, index) => (
-                <button key={index} value={value} onClick={handleButtonClick}>{value}</button>
-            ))}
-            <button onClick={clearInput}>C</button>
-            <button onClick={calculateResult}>=</button>
-          </div>
-        </div>
-        <h1>Product Catalog</h1>
-        <ProductList products={products} />
+      <div id="POS-Home">
+        <MainNavigation />
       </div>
   );
 }
---
+++ POS_App/POS_UI/POSClient/src/Components/Layout/MainNavigation.tsx
@@ -0,0 +1,124 @@
+﻿import React, {useState} from 'react';
+import "./MainNavigation.scss";
+import {FaBars, FaQuestionCircle, 
+    FaStore, FaUserAlt, FaSyncAlt, 
+    FaCircle, FaCashRegister, FaShoppingBasket,
+    FaMoneyBillWave, FaClipboardList, FaCalendarAlt,
+    FaChartLine, FaCreditCard, FaFileInvoiceDollar,
+    FaReceipt, FaSearchDollar, FaTruck} from "react-icons/fa";
+
+const MainNavigation: React.FC = () => {
+    const [showMenu, setShowMenu] = useState(false);
+    const [internetStatus] = useState(true);
+
+    const toggleLeftMenu = () => {
+        setShowMenu(!showMenu);
+    };
+    
+    return (
+        <>
+            <div id="POS-Top-Navigation">
+                <div className="POS-Top-Navigation-Left">
+                    <div className="Hamburger-Menu Action-Group" onClick={toggleLeftMenu}>
+                        <FaBars />
+                    </div>
+                    <div className="POS-Cash-Register Action-Group">
+                        <FaCashRegister />
+                        <span className="Cash-Register-Text">Cash Register</span>
+                    </div>
+                    <div className="POS-Offline-Orders Action-Group">
+                        <FaShoppingBasket />
+                        <span className="Offline-Orders-Text">Offline Orders (1)</span>
+                    </div>
+                </div>
+                <div className="POS-Top-Navigation-Right">
+                    <div className="POS-Internet-Status Action-Group">
+                        <FaCircle className={`${internetStatus ? 'online' : 'offline'}`} />
+                        <span className="Internet-Status-Text">Online</span>
+                    </div>
+                    <div className="POS-Sync-Area Action-Group">
+                        <FaSyncAlt />
+                        <span className="POS-Sync-Label">Sync POS</span>
+                    </div>
+                    <div className="POS-Logout-Area Action-Group">
+                        <FaUserAlt />
+                        <span className="POS-Logout-Label">Logout: Jorge Contreras</span>
+                    </div>
+                    <div className="POS-Branch-Area Action-Group">
+                        <FaStore />
+                        <span className="POS-Branch-Label">Main Branch</span>
+                    </div>
+                    <div className="POS-Help-Area Action-Group">
+                        <FaQuestionCircle />
+                    </div>
+                </div>
+            </div>
+            <div id="POS-Drop-Navigation" className={`${showMenu ? 'Show-Side-Menu' : ''}`}>
+                <div className="Drop-Menu-Options">
+                    <a href="#" className="Options-Item">
+                        <FaMoneyBillWave className="Option-Icon" />
+                        <span className="Options-Item-Label">New Sale</span>
+                    </a>
+                    <a href="#" className="Options-Item">
+                        <FaClipboardList className="Option-Icon" />
+                        <span className="Options-Item-Label">List Sales</span>
+                    </a>
+                    <a href="#" className="Options-Item">
+                        <FaCalendarAlt className="Option-Icon" />
+                        <span className="Options-Item-Label">New Layby</span>
+                    </a>
+                    <a href="#" className="Options-Item">
+                        <FaClipboardList className="Option-Icon" />
+                        <span className="Options-Item-Label">List Laybys</span>
+                    </a>
+                    <a href="#" className="Options-Item">
+                        <FaFileInvoiceDollar className="Option-Icon" />
+                        <span className="Options-Item-Label">New Quote</span>
+                    </a>
+                    <a href="#" className="Options-Item">
+                        <FaClipboardList className="Option-Icon" />
+                        <span className="Options-Item-Label">List Quotes</span>
+                    </a>
+                    <a href="#" className="Options-Item">
+                        <FaReceipt className="Option-Icon" />
+                        <span className="Options-Item-Label">No Receipt</span>
+                    </a>
+                    <a href="#" className="Options-Item">
+                        <FaSearchDollar className="Option-Icon" />
+                        <span className="Options-Item-Label">Search Orders</span>
+                    </a>
+                    <a href="#" className="Options-Item">
+                        <FaClipboardList className="Option-Icon" />
+                        <span className="Options-Item-Label">List Returns</span>
+                    </a>
+                    <a href="#" className="Options-Item">
+                        <FaTruck className="Option-Icon" />
+                        <span className="Options-Item-Label">New Transfer</span>
+                    </a>
+                    <a href="#" className="Options-Item">
+                        <FaClipboardList className="Option-Icon" />
+                        <span className="Options-Item-Label">List Transfers</span>
+                    </a>
+                    <a href="#" className="Options-Item">
+                        <FaCreditCard className="Option-Icon" />
+                        <span className="Options-Item-Label">New Purchase Order</span>
+                    </a>
+                    <a href="#" className="Options-Item">
+                        <FaCalendarAlt className="Option-Icon" />
+                        <span className="Options-Item-Label">List Purchases</span>
+                    </a>
+                    <a href="#" className="Options-Item">
+                        <FaCashRegister className="Option-Icon" />
+                        <span className="Options-Item-Label">Close Register</span>
+                    </a>
+                    <a href="#" className="Options-Item">
+                        <FaChartLine className="Option-Icon" />
+                        <span className="Options-Item-Label">Reports</span>
+                    </a>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default MainNavigation;

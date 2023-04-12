f6ab1828404f610f1e9f83bb17d4232d43265409
Jorge Contreras <jorge@lecontre.co.nz>
2023-04-12 21:13:07 +1200

    Product Summary Styles

---
+++ POS_App/POS_UI/POSClient/src/Components/Layout/Product/ProductLineItem.tsx
@@ -0,0 +1,23 @@
+﻿import React from 'react';
+import {FaTimes} from "react-icons/fa";
+
+const ProductLineItem: React.FC = () => {
+    return (
+        <div className="Product-Line-Item">
+            <div className="Product-Summary-Column Code">1401_Belt</div>
+            <div className="Product-Summary-Column Product">Mesh Belt - Black</div>
+            <div className="Product-Summary-Column Options"></div>
+            <div className="Product-Summary-Column Quantity">1</div>
+            <div className="Product-Summary-Column Take-Now">1</div>
+            <div className="Product-Summary-Column Unit-Price">15.00</div>
+            <div className="Product-Summary-Column Discount">0.00</div>
+            <div className="Product-Summary-Column Amount">15.00</div>
+            <div className="Product-Summary-Column Remove">
+                <FaTimes />
+            </div>
+        </div>
+    );
+}
+
+
+export default ProductLineItem;
--- POS_App/POS_UI/POSClient/src/Components/Layout/Product/ProductSummary.tsx
+++ POS_App/POS_UI/POSClient/src/Components/Layout/Product/ProductSummary.tsx
@@ -1,33 +1,25 @@
 ﻿import React from 'react';
+import ProductLineItem from "./ProductLineItem";
 import "./ProductSummary.scss";
-import {FaTimes} from "react-icons/fa";
 
 const ProductSummary: React.FC = () => {
     return (
         <div id="Product-Summary">
-            <div className="title-roll">
-                <div className="cell">Code</div>
-                <div className="cell">Product</div>
-                <div className="cell">Options</div>
-                <div className="cell">Order Qty</div>
-                <div className="cell">Take NowQty</div>
-                <div className="cell">Unit Price</div>
-                <div className="cell">Total Discount</div>
-                <div className="cell">Amount NZD</div>
-                <div className="cell"></div>
+            <div className="Product-Summary-Header">
+                <div className="Product-Summary-Column Code">Code</div>
+                <div className="Product-Summary-Column Product">Product</div>
+                <div className="Product-Summary-Column Options">Options</div>
+                <div className="Product-Summary-Column Quantity">Order Qty</div>
+                <div className="Product-Summary-Column Take-Now">Take NowQty</div>
+                <div className="Product-Summary-Column Unit-Price">Unit Price</div>
+                <div className="Product-Summary-Column Discount">Total Discount</div>
+                <div className="Product-Summary-Column Amount">Amount NZD</div>
+                <div className="Product-Summary-Column Remove"></div>
             </div>
-            <div className="row">
-                <div className="cell update">1401_Belt</div>
-                <div className="cell update">Mesh Belt - Black</div>
-                <div className="cell update"></div>
-                <div className="cell update number">1</div>
-                <div className="cell update number">1</div>
-                <div className="cell update money">15.00</div>
-                <div className="cell update money">0.00</div>
-                <div className="cell money">15.00</div>
-                <div className="cell">
-                    <FaTimes />
-                </div>
+            <div className="Product-Summary-Body">
+                <ProductLineItem />
+                <ProductLineItem />
+                <ProductLineItem />
             </div>
         </div> 
     )
